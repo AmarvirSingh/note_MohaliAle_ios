4a427381422d60862f161a67d72ddf53bca08657
simranaug24 <simranaug24@gmail.com>
2021-02-02 16:31:35 +0530

    cell modification

--- note_MohaliAle_ios/VC/CategoryTVC.swift
+++ note_MohaliAle_ios/VC/CategoryTVC.swift
@@ -53,23 +53,30 @@ class CategoryTVC: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return category.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "category_cell", for: indexPath)
 
-        // Configure the cell...
+        cell.textLabel?.text = category[indexPath.row].catName
+        cell.textLabel?.textColor = .lightGray
+        cell.detailTextLabel?.textColor = .lightGray
+        cell.detailTextLabel?.text = "\(category[indexPath.row].notes?.count ?? 0)"
+        cell.imageView?.image = UIImage(systemName: "category")
+        cell.selectionStyle = .none
+
+    
 
         return cell
     }
-    */
+
 
     /*
     // Override to support conditional editing of the table view.
