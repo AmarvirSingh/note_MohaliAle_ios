0e509ca68cfcd508e894f234a04a4bf51f06e3ba
Amarvir <amarvirgill06@gmail.com>
2021-02-03 22:50:16 +0530

    outlets taken form the controller

--- note_MohaliAle_ios/VC/NotesVC.swift
+++ note_MohaliAle_ios/VC/NotesVC.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class NotesVC: UIViewController {
+    
+    //MARK: - outlets of ther view controller
+    @IBOutlet weak var showLocationBtn: UIButton!
+    @IBOutlet weak var noteImage: UIImageView!
+    @IBOutlet weak var noteTextView: UITextView!
+    @IBOutlet weak var titleTextField: UITextField!
+    
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,14 +25,6 @@ class NotesVC: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  
 
 }
