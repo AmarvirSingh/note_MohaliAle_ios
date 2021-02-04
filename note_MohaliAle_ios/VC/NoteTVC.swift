7758d353f7f53a28e18d44020e3188c7ca53ca00
TanvinderKaur <tanvinderk1996@gmail.com>
2021-02-03 22:05:30 -0800

    delete note

--- note_MohaliAle_ios/VC/NoteTVC.swift
+++ note_MohaliAle_ios/VC/NoteTVC.swift
@@ -89,6 +89,15 @@ class NoteTVC: UITableViewController, UISearchBarDelegate {
         }
         tableView.reloadData()
     }
+    
+    
+    //Mark: -deletion of notes
+    /// deleting the  notes from context
+
+    /// - Parameter note: note defined in Core Data
+    func deleteNote(note: Note) {
+        context.delete(note)
+    }
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
